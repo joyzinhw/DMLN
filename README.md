f50c42b36edc852dcc4f7164818d34a6ad60c1fe
Joyce Moura <joyzinhw@gmail.com>
2025-07-09 13:03:36 -0300

    add: relatorio

--- README.md
+++ README.md
@@ -1,8 +1,63 @@
-# Detection of Malignant Lung Nodules
-
-## requisitos
+## APLICAÇÃO DE CNNs NA CLASSIFICAÇÃO DO CARCINOMA PULMONAR DE CÉLULAS NÃO PEQUENASrequisitos
 
 * python (version 3.13.0 or later)
+* [dataset dividido em treino e teste](https://drive.google.com/drive/folders/1bAshdY1GdLHfQswjQnLCSHxjEIaQXX7Q?usp=drive_link)
+* [dataset divido por categoria + cvs com dados do dataset](https://drive.google.com/file/d/1Tryg4cN30dSADB6r-yJrz-2TpBZJvpFD/view?usp=sharing)
 * ```
   pip install -r requirements.txt
   ```
+
+## OBJETIVO GERAL
+
+Aplicar e avaliar modelos de CNNs na classificação do carcinoma pulmonar de não pequenas células em imagens de tomografia computadorizada, a fim de investigar seu desempenho como ferramenta de apoio ao diagnóstico.
+
+Objetivos Especificos
+
+1. Realizar o pré-processamento do conjunto de dados;
+2. Utilizar técnicas de aumento de dados:
+3. Treinar e validar CNNs, utilizando técnicas de ajuste de hiperparâmetros;
+4. Avaliar o desempenho dos modelos aplicados e compará-los com a literatura;
+5. Analisar e discutir os resultados, destacando os benefícios e limitações do uso de CNNs.
+
+## MOTIVAÇÃO
+
+
+Câncer pulmonar de células não pequenas (CPCNP), responsável por cerca de 85% dos casos.
+
+- Neste segundo grupo estão incluídos os subtipos adenocarcinoma, carcinoma de células escamosas e carcinoma de grandes células.
+- O principal fator de risco para o desenvolvimento desse tipo de tumor é o tabagismo, associado a cerca de 85% dos casos diagnosticados.
+
+LIMITAÇÕES E TRABALHOS FUTUROS:
+a falta de no dataset carcinoma de grandes células.
+implementar carcinoma de grandes células na classificação
+
+# Dataset Info
+
+desbalanceamento de classes:
+total: 95 pacientes e 330 nódulos anotados, 308 fatias
+
+0  -   Benignos (103)
+
+1  -   Adenocarcinoma (172)
+
+2   -   Carcinoma de Células Escamosas (33)
+
+técnicas sugeridas pela literatura para desbalanciamento:  GANs & Autoencoders
+
+o dataset 2D ja veio particionado assim,:
+
+TRAIN : 80%
+
+Pasta: dataset/train/0 - 83 imagens
+
+Pasta: dataset/train/1 - 137 imagens
+
+Pasta: dataset/train/2 - 26 imagens
+
+TEST : 20%
+
+Pasta: dataset/test/0 - 20 imagens
+
+Pasta: dataset/test/1 - 35 imagens
+
+Pasta: dataset/test/2 - 7 imagens
---
+++ classification/train.py
@@ -0,0 +1,171 @@
+import torch.nn as nn
+from torchvision import transforms, datasets
+import json
+import argparse
+import os
+import torch.optim as optim
+import torch
+from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sn
+
+from resnet50 import resnet50
+from train_utils import get_params_groups, create_lr_scheduler, train_one_epoch, evaluate
+from sklearn.metrics import confusion_matrix
+
+
+def main(args):
+    os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    tb_writer = SummaryWriter()
+
+    data_transform = {
+        "train": transforms.Compose([
+            transforms.Resize((448, 448)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.RandomRotation((0, 360)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+        ]),
+        "val": transforms.Compose([
+            transforms.Resize((448, 448)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+        ])
+    }
+
+   
+    train_dir = os.path.join(args.data_path, "BMP_classification/", "train")
+    val_dir = os.path.join(args.data_path, "BMP_classification/", "test")
+
+    if not os.path.isdir(train_dir):
+        raise FileNotFoundError(f"Diretório de treino não encontrado: {train_dir}")
+    if not os.path.isdir(val_dir):
+        raise FileNotFoundError(f"Diretório de validação não encontrado: {val_dir}")
+
+    train_dataset = datasets.ImageFolder(root=train_dir, transform=data_transform["train"])
+    val_dataset = datasets.ImageFolder(root=val_dir, transform=data_transform["val"])
+
+    train_num = len(train_dataset)
+    batch_size = args.batch_size
+    num_workers = 8
+    print(f'Using {num_workers} dataloader workers every process')
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        pin_memory=True,
+        num_workers=num_workers
+    )
+
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        pin_memory=True,
+        num_workers=num_workers
+    )
+
+    model = resnet50(num_classes=args.num_classes)
+    model.to(device)
+
+    pg = get_params_groups(model, weight_decay=args.wd)
+
+    
+    optimizer = optim.AdamW(pg, lr=args.lr, weight_decay=args.wd)
+
+    lr_scheduler = create_lr_scheduler(optimizer, len(train_loader), args.epochs,
+                                       warmup=True, warmup_epochs=10)
+
+    best_acc = 0.
+    best_train_acc = 0.
+    best_kappa = 0.
+
+    for epoch in range(args.epochs):
+        train_loss, train_acc, train_kappa1, train_truee, train_predd = train_one_epoch(
+            model=model,
+            optimizer=optimizer,
+            data_loader=train_loader,
+            device=device,
+            epoch=epoch,
+            lr_scheduler=lr_scheduler
+        )
+        print(f"train_kappa= {train_kappa1:.4f}")
+
+        if train_acc > best_train_acc:
+            conf_matrix = confusion_matrix(train_truee, train_predd)
+            data = {'True label': train_truee, 'Predict label': train_predd}
+            df = pd.DataFrame(data, columns=['True label', 'Predict label'])
+            confmtpd = pd.crosstab(df['True label'], df['Predict label'], dropna=False)
+            print(f"Confusion matrix with pandas:\n{confmtpd}")
+            plt.figure()
+            sn.heatmap(confmtpd, annot=True, cmap='Greens', fmt='d')
+            plt.show()
+
+            best_train_acc = train_acc
+
+        val_loss, val_acc, val_kappa1, val_truee, val_predd = evaluate(
+            model=model,
+            data_loader=val_loader,
+            device=device,
+            epoch=epoch
+        )
+        print(f"val_kappa= {val_kappa1:.4f}")
+
+        tags = ["train_loss", "train_acc", "val_loss", "val_acc", "learning_rate", "train_kappa", "val_kappa"]
+        tb_writer.add_scalar(tags[0], train_loss, epoch)
+        tb_writer.add_scalar(tags[1], train_acc, epoch)
+        tb_writer.add_scalar(tags[2], val_loss, epoch)
+        tb_writer.add_scalar(tags[3], val_acc, epoch)
+        tb_writer.add_scalar(tags[4], optimizer.param_groups[0]["lr"], epoch)
+        tb_writer.add_scalar(tags[5], train_kappa1, epoch)
+        tb_writer.add_scalar(tags[6], val_kappa1, epoch)
+
+        if best_acc < val_acc:
+            save_path = "classfication_result/cancer_resnet50.pth"
+            torch.save(model.state_dict(), save_path)
+            print(f"Model saved to {save_path}")
+
+            val_conf_matrix = confusion_matrix(val_truee, val_predd)
+            val_data = {'True label': val_truee, 'Predict label': val_predd}
+            val_df = pd.DataFrame(val_data, columns=['True label', 'Predict label'])
+            val_confmtpd = pd.crosstab(val_df['True label'], val_df['Predict label'], dropna=False)
+            print(f"Confusion matrix with pandas:\n{val_confmtpd}")
+
+            plt.figure()
+            sn.heatmap(val_confmtpd, annot=True, cmap='Greens', fmt='d')
+            plt.savefig('classfication_result/cancer_resnet50_confusion_matrix.png')
+
+
+            best_acc = val_acc
+
+        if best_kappa < val_kappa1:
+            best_kappa = val_kappa1
+
+        print(f"best_train_acc = {best_train_acc:.4f}")
+        print(f"best_val_acc = {best_acc:.4f}")
+        print(f"best_val_kappa = {best_kappa:.4f}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_classes', type=int, default=3)
+    parser.add_argument('--epochs', type=int, default=200)
+    parser.add_argument('--batch-size', type=int, default=8)
+    parser.add_argument('--lr', type=float, default=5e-4)
+    parser.add_argument('--wd', type=float, default=0.05)
+    parser.add_argument('--data-path', type=str,
+                        default="BMP_classification/")
+    parser.add_argument('--weights', type=str,
+                        default='',
+                        help='initial weights path')
+    parser.add_argument('--freeze-layers', type=bool, default=False)
+    parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
+
+    opt = parser.parse_args()
+
+    main(opt)
